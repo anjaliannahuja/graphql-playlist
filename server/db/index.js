f6f779181ab91dc5238d88a2eb24d8f57a6c3204
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-07-11 09:40:35 -0500

    Starting mutations

---
+++ server/db/index.js
@@ -0,0 +1,16 @@
+const pg = require('pg');
+
+require('dotenv').config();
+
+//connect to postgres local db with credentials
+const client = new pg.Client(process.env.DATABASE_URL);
+
+client.connect(err => {
+  if (err) {
+    console.error('error: ', err);
+  } else {
+    console.log('CONNECTED to local postgres db');
+  }
+});
+
+module.exports = client;
---
+++ server/db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS gql_ninja;
+
+CREATE DATABASE gql_ninja;
+
+\c gql_ninja;
+
+CREATE TABLE authors (
+  author_id BIGSERIAL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  age integer NOT NULL
+);
+
+CREATE TABLE books (
+  book_id BIGSERIAL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  genre varchar(255),
+  author_id BIGSERIAL NOT NULL references authors(author_id)
+);
+
+
+/*  Execute this file from the command line by typing:
+ *    psql -U anjaliahuja < ./server/db/schema.sql
+ *  to create the database and the tables.*/
+
+--  
